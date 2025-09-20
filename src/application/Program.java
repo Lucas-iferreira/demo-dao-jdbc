aceddd4b3a7a5932a91b17a9ebbfd914411c06d6
Lucas <lucas.iferreir@gmail.com>
2025-09-20 20:24:24 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(1, "Lucas", "lucas@gmail.com", new Date(), 1200.00, department);
+
+        
+        System.out.println(seller);
     }
 }
